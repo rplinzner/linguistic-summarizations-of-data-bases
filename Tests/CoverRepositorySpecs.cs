a8936a496c664e693c3fd83156df12fb756a0bc6
Michał Chudzik <michalchudzik@live.com>
2019-05-18 17:07:59 +0200

    [Refactor] Data library specs

--- Tests/CoverRepositorySpecs.cs
+++ Tests/CoverRepositorySpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,28 +9,86 @@ namespace Tests
     [TestClass]
     public class CoverRepositorySpecs
     {
+        private List<Cover> objects;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            objects = CoverRepository.All();
+        }
         [TestMethod]
         public void AllSpec()
         {
-            var temp = CoverRepository.All();
-            Assert.AreEqual(temp.Count, 11001);
+            Assert.AreEqual(objects.Count, 11001);
         }
 
         [TestMethod]
-        public void Domains()
+        public void ElevationDomain()
         {
-            var objects = CoverRepository.All();
-            Console.Out.WriteLine("Elevation: Min: {0}, Max {1}", objects.Select(c => c.Elevation).Min(), objects.Select(c => c.Elevation).Max());
-            Console.Out.WriteLine("Aspect: Min: {0}, Max {1}", objects.Select(c => c.Aspect).Min(), objects.Select(c => c.Aspect).Max());
-            Console.Out.WriteLine("slope: Min: {0}, Max {1}", objects.Select(c => c.Slope).Min(), objects.Select(c => c.Slope).Max());
-            Console.Out.WriteLine("horizontal_distance_to_hydrology: Min: {0}, Max {1}", objects.Select(c => c.HorizontalDistanceToHydrology).Min(), objects.Select(c => c.HorizontalDistanceToHydrology).Max());
-            Console.Out.WriteLine("vertical_distance_to_hydrology: Min: {0}, Max {1}", objects.Select(c => c.VerticalDistanceToHydrology).Min(), objects.Select(c => c.VerticalDistanceToHydrology).Max());
-            Console.Out.WriteLine("horizontal_distance_to_roadways: Min: {0}, Max {1}", objects.Select(c => c.HorizontalDistanceToRoadways).Min(), objects.Select(c => c.HorizontalDistanceToRoadways).Max());
-            Console.Out.WriteLine("hillshade_9am: Min: {0}, Max {1}", objects.Select(c => c.Hillshade9Am).Min(), objects.Select(c => c.Hillshade9Am).Max());
-            Console.Out.WriteLine("hillshade_noon: Min: {0}, Max {1}", objects.Select(c => c.HillshadeNoon).Min(), objects.Select(c => c.HillshadeNoon).Max());
-            Console.Out.WriteLine("hillshade_3pm: Min: {0}, Max {1}", objects.Select(c => c.Hillshade3Pm).Min(), objects.Select(c => c.Hillshade3Pm).Max());
-            Console.Out.WriteLine("horizontal_distance_to_fire_points: Min: {0}, Max {1}", objects.Select(c => c.HorizontalDistanceToFirePoints).Min(), objects.Select(c => c.HorizontalDistanceToFirePoints).Max());
-            Console.Out.WriteLine("cover_type: Min: {0}, Max {1}", objects.Select(c => c.CoverType).Min(), objects.Select(c => c.CoverType).Max());
+            Assert.AreEqual(objects.Select(c => c.Elevation).Min(), 1863);
+            Assert.AreEqual(objects.Select(c => c.Elevation).Max(), 3849);
+        }
+
+        [TestMethod]
+        public void SlopeDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.Slope).Min(), 0);
+            Assert.AreEqual(objects.Select(c => c.Slope).Max(), 52);
+        }
+
+        [TestMethod]
+        public void AspectDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.Aspect).Min(), 0);
+            Assert.AreEqual(objects.Select(c => c.Aspect).Max(), 360);
+        }
+        [TestMethod]
+        public void HorizontalDistanceToHydrologyDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.HorizontalDistanceToHydrology).Min(), 0);
+            Assert.AreEqual(objects.Select(c => c.HorizontalDistanceToHydrology).Max(), 1343);
+        }
+        [TestMethod]
+        public void VerticalDistanceToHydrologyDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.VerticalDistanceToHydrology).Min(), -146);
+            Assert.AreEqual(objects.Select(c => c.VerticalDistanceToHydrology).Max(), 554);
+        }
+        [TestMethod]
+        public void HorizontalDistanceToRoadwaysDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.HorizontalDistanceToRoadways).Min(), 0);
+            Assert.AreEqual(objects.Select(c => c.HorizontalDistanceToRoadways).Max(), 6890);
+        }
+        [TestMethod]
+        public void Hillshade9amDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.Hillshade9Am).Min(), 0);
+            Assert.AreEqual(objects.Select(c => c.Hillshade9Am).Max(), 254);
+        }
+        [TestMethod]
+        public void HillshadeNoonDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.HillshadeNoon).Min(), 99);
+            Assert.AreEqual(objects.Select(c => c.HillshadeNoon).Max(), 254);
+        }
+        [TestMethod]
+        public void Hillshade3pmDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.Hillshade3Pm).Min(), 0);
+            Assert.AreEqual(objects.Select(c => c.Hillshade3Pm).Max(), 248);
+        }
+        [TestMethod]
+        public void HorizontalDistanceToFirePointsDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.HorizontalDistanceToFirePoints).Min(), 30);
+            Assert.AreEqual(objects.Select(c => c.HorizontalDistanceToFirePoints).Max(), 6853);
+        }
+        [TestMethod]
+        public void CoverTypeDomain()
+        {
+            Assert.AreEqual(objects.Select(c => c.CoverType).Min(), 1);
+            Assert.AreEqual(objects.Select(c => c.CoverType).Max(), 7);
         }
     }
 }
