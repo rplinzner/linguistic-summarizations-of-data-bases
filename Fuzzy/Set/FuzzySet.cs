aee67db5642feff38abc66925e00b54457631019
Michał Chudzik <michalchudzik@live.com>
2019-05-18 16:08:24 +0200

    [Add] Base for Fuzzy Set operations - without specs

--- Fuzzy/Set/FuzzySet.cs
+++ Fuzzy/Set/FuzzySet.cs
@@ -1,6 +1,35 @@
-﻿namespace Fuzzy.Set
+﻿using Fuzzy.Function;
+using System;
+
+namespace Fuzzy.Set
 {
-    class FuzzySet
+    public class FuzzySet
     {
+        IFunction MembershipFunction { get; set; }
+
+        public FuzzySet(IFunction membershipFunction)
+        {
+            MembershipFunction = membershipFunction;
+        }
+
+        public double Membership(double x)
+        {
+            return MembershipFunction.Value(x);
+        }
+
+        public double Union(FuzzySet other, double x)
+        {
+            return Math.Max(Membership(x), other.Membership(x));
+        }
+
+        public double Intersection(FuzzySet other, double x)
+        {
+            return Math.Min(Membership(x), other.Membership(x));
+        }
+
+        public bool Equals(FuzzySet other, double x)
+        {
+            return Membership(x).Equals(other.Membership(x));
+        }
     }
 }
---
+++ FuzzySpecs/FuzzySetSpecs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Fuzzy.Set;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FuzzySpecs
+{
+    [TestClass]
+    public class FuzzySetSpecs
+    {
+        [TestMethod]
+        public void SumOfTwoFuzzySets()
+        {
+        }
+    }
+}
