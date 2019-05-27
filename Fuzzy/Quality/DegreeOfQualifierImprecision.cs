1c4111ef7a1a289454857ef4509294c33682e221
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-05-27 14:53:27 +0200

    [Add] T9 - Degree Of Qualifier Imprecision

--- Fuzzy/Quality/DegreeOfQualifierImprecision.cs
+++ Fuzzy/Quality/DegreeOfQualifierImprecision.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fuzzy.Summarizer;
 
 namespace Fuzzy.Quality
 {
     public class DegreeOfQualifierImprecision : IDegree
     {
-        public DegreeOfQualifierImprecision()
+        public List<Qualifier> Qualifiers { get; set; } = new List<Qualifier>();
+        public DegreeOfQualifierImprecision() { }
+        public DegreeOfQualifierImprecision(List<Qualifier> qualifiers)
         {
-
+            Qualifiers = qualifiers;
         }
         public double Call()
         {
-            throw new NotImplementedException();
+            if (Qualifiers.Count == 0) return 1;
+            double result = 1;
+            foreach (Qualifier qualifier in Qualifiers)
+            {
+                result *= qualifier.FuzzySet.DegreeOfFuzziness();
+            }
+
+            result = Math.Pow(result, 1.0 / Qualifiers.Count);
+            return 1.0 - result;
         }
     }
 }
