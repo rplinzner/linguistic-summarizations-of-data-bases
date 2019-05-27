212414e4e8c1992967b4b4b3191d6f2143f5e368
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-05-27 14:16:43 +0200

    [Add] T8 - Degree Of Summarizer Cardinality

---
+++ Fuzzy/Quality/DegreeOfSummarizerCardinality.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzzy.Quality
+{
+    public class DegreeOfSummarizerCardinality : IDegree
+    {
+        public List<Summarizer.Summarizer> Summarizers { get; set; }
+        public DegreeOfSummarizerCardinality(List<Summarizer.Summarizer> summarizers)
+        {
+            Summarizers = Summarizers;
+        }
+        public double Call()
+        {
+            double result = 1.0;
+            foreach (Summarizer.Summarizer summarizer in Summarizers)
+            {
+                result *= (summarizer.FuzzySet.MembershipFunction.Cardinality() /
+                          summarizer.FuzzySet.MembershipFunction.DomainCardinality());
+            }
+            result = Math.Pow(result, Summarizers.Count);
+            return 1.0 - result;
+        }
+    }
+}
