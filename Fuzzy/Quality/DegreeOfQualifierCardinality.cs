2e920500f9adff01a80ca4b8ebfa75f459c4db47
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-05-27 14:46:15 +0200

    [Add]  T10 - Degree Of Qualifier Cardinality

---
+++ Fuzzy/Quality/DegreeOfQualifierCardinality.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fuzzy.Summarizer;
+
+namespace Fuzzy.Quality
+{
+    public class DegreeOfQualifierCardinality : IDegree
+    {
+        public Qualifier Qualifier { get; set; }
+
+        public DegreeOfQualifierCardinality()
+        {
+
+        }
+        public DegreeOfQualifierCardinality(Qualifier qualifier)
+        {
+            Qualifier = qualifier;
+        }
+        public double Call()
+        {
+            if (Qualifier == null) return 1;
+            return 1.0 - (Qualifier.FuzzySet.MembershipFunction.Cardinality() / Qualifier.FuzzySet.MembershipFunction.DomainCardinality());
+        }
+    }
+}
---
+++ Fuzzy/Quality/DegreeOfQualifierImprecision.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzzy.Quality
+{
+    public class DegreeOfQualifierImprecision : IDegree
+    {
+        public DegreeOfQualifierImprecision()
+        {
+
+        }
+        public double Call()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
