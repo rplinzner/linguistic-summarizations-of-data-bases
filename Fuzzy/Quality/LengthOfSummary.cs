bac03f95aced2199e208813a79b71006c4f02a50
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-05-27 13:45:16 +0200

    [Add] T5 - Length of Summary

---
+++ Fuzzy/Quality/LengthOfSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzzy.Quality
+{
+    public class LengthOfSummary : IDegree
+    {
+        public List<Summarizer.Summarizer> Summarizers { get; set; }
+        public LengthOfSummary(List<Summarizer.Summarizer> summarizers)
+        {
+            Summarizers = summarizers;
+        }
+
+        public double Call()
+        {
+            return 2.0 * Math.Pow(0.5, Summarizers.Count);
+        }
+    }
+}
