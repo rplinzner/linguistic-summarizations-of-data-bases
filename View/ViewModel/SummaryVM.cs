b45b676102ccb175aea0ee34262eb37454a46b60
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-05-29 18:51:40 +0200

    [Refactor] Summary VM

--- View/ViewModel/SummaryVM.cs
+++ View/ViewModel/SummaryVM.cs
@@ -54,7 +54,7 @@ namespace View.ViewModel
         public int SelectedSubject { get; set; }
 
         public ObservableCollection<Summarizer> Qualifiers { get; set; } = new ObservableCollection<Summarizer>();
-        public Summarizer SelectedQualifiers { get; set; }
+        public Summarizer SelectedQualifier { get; set; }
 
         public ObservableCollection<string> Conjunctions { get; set; }
         public string SelectedConjunction { get; set; }
@@ -107,13 +107,11 @@ namespace View.ViewModel
                 Qualifiers.Add(new Summarizer("", null));
             }
 
-
-
         }
 
         private List<int> ExtractColumn(AttributesListVm attr)
         {
-            
+
             switch (attr.Name)
             {
                 case "Elevation":
@@ -140,244 +138,129 @@ namespace View.ViewModel
 
             return null;
         }
+     
 
-        public void Summarize()
+        private void Summarize()
         {
+            if (string.IsNullOrEmpty(SelectedSummarizer.Label))
+            {
+                MessageBox.Show("Please Choose Summarizer");
+                return;
+            }
+
+            List<int> ValuesForSummarizer = ExtractColumn(SelectedAttribute);
+            List<int> ValuesForSummarizer2 = null;
+
+            bool hasQualifier = SelectedQualifier != null && !string.IsNullOrEmpty(SelectedQualifier.Label);
+            bool hasTwoSummarizers = SelectedSecondSummarizer != null && !string.IsNullOrEmpty(SelectedSecondSummarizer.Label);
+            if (hasTwoSummarizers) //if second summarizer is selected
+            {
+                ValuesForSummarizer2 = ExtractColumn(SelectedSecondAttribute);
+            }
+
             CovertypeConverter convert = new CovertypeConverter();
             List<Cover> subject = Parent.Covers.Where(x => x.CoverType == SelectedSubject).ToList();
-            if (OneSummarizerWithoutQualifier())
-            {
-                OneSummarizer(subject, convert);
-                return; 
-            }
-            if (TwoSummarizersWithoutQualifiers())
-            {
-                TwoSummarizers(subject, convert);
-                return;
-            }
 
-            if (OneSummarizerWithQualifier())
-            {
-                OneSummarizerWithQ(subject, convert);
-                return;
-            }
 
-            if (TwoSummarizersWithQualifier())
-            {
-                TwoSummarizersWithQ(subject, convert);
-                return;
-            }
-           
-        }
-
-        private bool OneSummarizerWithoutQualifier() =>
-            SelectedSummarizer != null && SelectedSecondSummarizer == null && SelectedQualifiers == null;
-
-        private bool TwoSummarizersWithoutQualifiers() =>
-            SelectedQualifiers == null && SelectedSecondSummarizer != null && SelectedSummarizer != null;
-        private bool OneSummarizerWithQualifier() => SelectedSummarizer != null && SelectedSecondSummarizer == null && SelectedQualifiers != null;
-        private bool TwoSummarizersWithQualifier() => SelectedQualifiers != null && SelectedSecondSummarizer != null && SelectedSummarizer != null;
-
-        private void OneSummarizer(List<Cover> subject, CovertypeConverter convert)
-        {
-            List<int> ValuesForSummarizer = ExtractColumn(SelectedAttribute);
             foreach (var quantifier in Quantifiers)
             {
-                string summarization = $"{quantifier} {convert.Convert(SelectedSubject, null, null, null)}";
-                summarization += $" ARE/HAVE {SelectedSummarizer}";
-                summarization = summarization.Replace("Trapezoidal", String.Empty);
-                summarization = summarization.Replace("Triangular", String.Empty);
-                Summary summary = new Summary()
-                {
-                    Description = summarization,
-                    T1 = Math.Round(new DegreeOfTruth()
-                    {
-                        Summarizer1 = SelectedSummarizer,
-                        Quantifier = quantifier,
-                        Operation = SelectedConjunction,
-                        ValuesForSummarizer1 = ValuesForSummarizer
-                    }.Call(), 2),
-                    T2 = Math.Round(new DegreeOfImprecision(new List<Summarizer>() { SelectedSummarizer }).Call(), 2),
-                    T5 = Math.Round(new LengthOfSummary(new List<Summarizer>() { SelectedSummarizer }).Call(), 2),
-                    T6 = Math.Round(new DegreeOfQuantifierImprecision(quantifier).Call(), 2),
-                    T7 = Math.Round(new DegreeOfQuantifierCardinality(quantifier).Call(), 2),
-                    T8 = Math.Round(new DegreeOfSummarizerCardinality(new List<Summarizer>() { SelectedSummarizer }).Call(), 2),
-                    T9 = Math.Round(new DegreeOfQualifierImprecision().Call(), 2),
-                    T10 = Math.Round(new DegreeOfQualifierCardinality().Call(), 2),
-                    T11 = Math.Round(new LengthOfQualifier().Call(), 2),
-                };
-                summary.T3 = Math.Round(new DegreeOfCoverage()
+                var sum = GetSummary(quantifier, hasQualifier, hasTwoSummarizers, convert);
+                var t1 = new DegreeOfTruth()
                 {
                     Summarizer1 = SelectedSummarizer,
+                    Quantifier = quantifier,
+                    Operation = SelectedConjunction,
                     ValuesForSummarizer1 = ValuesForSummarizer
-                }.Call(), 2);
-                summary.T4 = Math.Round(new DegreeOfAppropriateness()
-                {
-                    T3 = summary.T3,
-                    Summarizer1 = SelectedSummarizer,
-                    ValuesForSummarizer1 = ValuesForSummarizer
-                }.Call(), 2);
-                summary.CalculateT();
-
-                Summaries.Add(summary);
-            }
-        }
-
-        private void TwoSummarizers(List<Cover> subject, CovertypeConverter convert)
-        {
-            List<int> ValuesForSummarizer1 = ExtractColumn(SelectedAttribute);
-            List<int> ValuesForSummarizer2 = ExtractColumn(SelectedSecondAttribute);
-
-            foreach (var quantifier in Quantifiers)
-            {
-                string summarization = $"{quantifier} {convert.Convert(SelectedSubject, null, null, null)}";
-                summarization += $" ARE/HAVE {SelectedSummarizer} {SelectedConjunction} {SelectedSecondSummarizer}";
-                summarization = summarization.Replace("Trapezoidal", String.Empty);
-                summarization = summarization.Replace("Triangular", String.Empty);
-                Summary summary = new Summary()
-                {
-                    Description = summarization,
-                    T1 = Math.Round(new DegreeOfTruth()
-                    {
-                        Summarizer1 = SelectedSummarizer,
-                        Summarizer2 = SelectedSecondSummarizer,
-                        Quantifier = quantifier,
-                        ValuesForSummarizer1 = ValuesForSummarizer1,
-                        ValuesForSummarizer2 = ValuesForSummarizer2,
-                        Operation = SelectedConjunction
-                    }.Call(), 2),
-                    T2 = Math.Round(new DegreeOfImprecision(new List<Summarizer>() { SelectedSummarizer, SelectedSecondSummarizer }).Call(), 2),
-                    T5 = Math.Round(new LengthOfSummary(new List<Summarizer>() { SelectedSummarizer, SelectedSecondSummarizer }).Call(), 2),
-                    T6 = Math.Round(new DegreeOfQuantifierImprecision(quantifier).Call(), 2),
-                    T7 = Math.Round(new DegreeOfQuantifierCardinality(quantifier).Call(), 2),
-                    T8 = Math.Round(new DegreeOfSummarizerCardinality(new List<Summarizer>() { SelectedSummarizer, SelectedSecondSummarizer }).Call(), 2),
-                    T9 = Math.Round(new DegreeOfQualifierImprecision().Call(), 2),
-                    T10 = Math.Round(new DegreeOfQualifierCardinality().Call(), 2),
-                    T11 = Math.Round(new LengthOfQualifier().Call(), 2),
                 };
-                summary.T3 = Math.Round(new DegreeOfCoverage()
-                {
-                    Summarizer1 = SelectedSummarizer,
-                    Summarizer2 = SelectedSecondSummarizer,
-                    ValuesForSummarizer1 = ValuesForSummarizer1,
-                    ValuesForSummarizer2 = ValuesForSummarizer2,
-                    Operation = SelectedConjunction
-                }.Call(), 2);
-                summary.T4 = Math.Round(new DegreeOfAppropriateness()
-                {
-                    T3 = summary.T3,
-                    Summarizer1 = SelectedSummarizer,
-                    Summarizer2 = SelectedSecondSummarizer,
-                    ValuesForSummarizer1 = ValuesForSummarizer1,
-                    ValuesForSummarizer2 = ValuesForSummarizer2
-                }.Call(), 2);
-                summary.CalculateT();
-
-                Summaries.Add(summary);
-            }
-        }
-
-        private void OneSummarizerWithQ(List<Cover> subject, CovertypeConverter convert)
-        {
-            List<int> ValuesForSummarizer = ExtractColumn(SelectedAttribute);
-
-            foreach (var quantifier in Quantifiers)
-            {
-                string summarization = $"{quantifier} {convert.Convert(SelectedSubject, null, null, null)}";
-                summarization += $" BEING/HAVING {SelectedQualifiers} ARE/HAVE {SelectedSummarizer}";
-                summarization = summarization.Replace("Trapezoidal", String.Empty);
-                summarization = summarization.Replace("Triangular", String.Empty);
-                Summary summary = new Summary()
-                {
-                    Description = summarization,
-                    T1 = Math.Round(new DegreeOfTruth()
-                    {
-                        Summarizer1 = SelectedSummarizer,
-                        Quantifier = quantifier,
-                        Qualifier = SelectedQualifiers,
-                        Operation = SelectedConjunction,
-                        ValuesForSummarizer1 = ValuesForSummarizer
-                    }.Call(), 2),
-                    T2 = Math.Round(new DegreeOfImprecision(new List<Summarizer>() { SelectedSummarizer }).Call(), 2),
-                    T5 = Math.Round(new LengthOfSummary(new List<Summarizer>() { SelectedSummarizer }).Call(), 2),
-                    T6 = Math.Round(new DegreeOfQuantifierImprecision(quantifier).Call(), 2),
-                    T7 = Math.Round(new DegreeOfQuantifierCardinality(quantifier).Call(), 2),
-                    T8 = Math.Round(new DegreeOfSummarizerCardinality(new List<Summarizer>() { SelectedSummarizer }).Call(), 2),
-                    T9 = Math.Round(new DegreeOfQualifierImprecision(new List<Summarizer>() { SelectedQualifiers }).Call(), 2),
-                    T10 = Math.Round(new DegreeOfQualifierCardinality(SelectedQualifiers).Call(), 2),
-                    T11 = Math.Round(new LengthOfQualifier(new List<Summarizer>() { SelectedQualifiers }).Call(), 2),
-                };
-                summary.T3 = Math.Round(new DegreeOfCoverage()
+                var t2 = new DegreeOfImprecision(new List<Summarizer>() { SelectedSummarizer });
+                var t3 = new DegreeOfCoverage()
                 {
                     Summarizer1 = SelectedSummarizer,
                     ValuesForSummarizer1 = ValuesForSummarizer,
-                    Qualifier = SelectedQualifiers
-                }.Call(), 2);
-                summary.T4 = Math.Round(new DegreeOfAppropriateness()
+                    Operation = SelectedConjunction
+                };
+                var t4 = new DegreeOfAppropriateness()
                 {
-                    T3 = summary.T3,
                     Summarizer1 = SelectedSummarizer,
                     ValuesForSummarizer1 = ValuesForSummarizer
-                }.Call(), 2);
-                summary.CalculateT();
+                };
+                var t5 = new LengthOfSummary(new List<Summarizer>() { SelectedSummarizer });
+                var t6 = new DegreeOfQuantifierImprecision(quantifier);
+                var t7 = new DegreeOfQuantifierCardinality(quantifier);
+                var t8 = new DegreeOfSummarizerCardinality(new List<Summarizer>() { SelectedSummarizer });
+                var t9 = new DegreeOfQualifierImprecision();
+                var t10 = new DegreeOfQualifierCardinality();
+                var t11 = new LengthOfQualifier();
+                if (hasQualifier) //If Qualifier Is Applied
+                {
+                    t1.Qualifier = SelectedQualifier;
+                    t3.Qualifier = SelectedQualifier;
+                    t9 = new DegreeOfQualifierImprecision(new List<Summarizer> { SelectedQualifier });
+                    t10 = new DegreeOfQualifierCardinality(SelectedQualifier);
+                    t11 = new LengthOfQualifier(new List<Summarizer>() { SelectedQualifier });
+                }
+                if (hasTwoSummarizers) //if second summarizer is selected
+                {
+                    t1.Summarizer2 = SelectedSecondSummarizer;
+                    t1.ValuesForSummarizer2 = ValuesForSummarizer2;
 
-                Summaries.Add(summary);
-            }
-        }
+                    t2.Summarizers.Add(SelectedSecondSummarizer);
 
-        private void TwoSummarizersWithQ(List<Cover> subject, CovertypeConverter convert)
-        {
-            List<int> ValuesForSummarizer1 = ExtractColumn(SelectedAttribute);
-            List<int> ValuesForSummarizer2 = ExtractColumn(SelectedSecondAttribute);
-            foreach (var quantifier in Quantifiers)
-            {
-                string summarization = $"{quantifier} {convert.Convert(SelectedSubject, null, null, null)}";
-                summarization += $" BEING/HAVING {SelectedQualifiers} ARE/HAVE {SelectedSummarizer} {SelectedConjunction} {SelectedSecondSummarizer}";
-                summarization = summarization.Replace("Trapezoidal", String.Empty);
-                summarization = summarization.Replace("Triangular", String.Empty);
+                    t3.Summarizer2 = SelectedSecondSummarizer;
+                    t3.ValuesForSummarizer2 = ValuesForSummarizer2;
+
+                    t4.Summarizer2 = SelectedSecondSummarizer;
+                    t4.ValuesForSummarizer2 = ValuesForSummarizer2;
+
+                    t5.Summarizers.Add(SelectedSecondSummarizer);
+
+                    t8.Summarizers.Add(SelectedSecondSummarizer);
+                }
+
+                t4.T3 = t3.Call();
+
                 Summary summary = new Summary()
                 {
-                    Description = summarization,
-                    T1 = Math.Round(new DegreeOfTruth()
-                    {
-                        Summarizer1 = SelectedSummarizer,
-                        Summarizer2 = SelectedSecondSummarizer,
-                        Quantifier = quantifier,
-                        Qualifier = SelectedQualifiers,
-                        ValuesForSummarizer1 = ValuesForSummarizer1,
-                        ValuesForSummarizer2 = ValuesForSummarizer2,
-                        Operation = SelectedConjunction
-                    }.Call(), 2),
-                    T2 = Math.Round(new DegreeOfImprecision(new List<Summarizer>() { SelectedSummarizer, SelectedSecondSummarizer }).Call(), 2),
-                    T5 = Math.Round(new LengthOfSummary(new List<Summarizer>() { SelectedSummarizer, SelectedSecondSummarizer }).Call(), 2),
-                    T6 = Math.Round(new DegreeOfQuantifierImprecision(quantifier).Call(), 2),
-                    T7 = Math.Round(new DegreeOfQuantifierCardinality(quantifier).Call(), 2),
-                    T8 = Math.Round(new DegreeOfSummarizerCardinality(new List<Summarizer>() { SelectedSummarizer, SelectedSecondSummarizer }).Call(), 2),
-                    T9 = Math.Round(new DegreeOfQualifierImprecision(new List<Summarizer>() { SelectedQualifiers }).Call(), 2),
-                    T10 = Math.Round(new DegreeOfQualifierCardinality(SelectedQualifiers).Call(), 2),
-                    T11 = Math.Round(new LengthOfQualifier(new List<Summarizer>() {SelectedQualifiers}).Call(), 2),
-                };
-                summary.T3 = Math.Round(new DegreeOfCoverage()
-                {
-                    Summarizer1 = SelectedSummarizer,
-                    Summarizer2 = SelectedSecondSummarizer,
-                    Qualifier = SelectedQualifiers,
-                    ValuesForSummarizer1 = ValuesForSummarizer1,
-                    ValuesForSummarizer2 = ValuesForSummarizer2,
-                    Operation = SelectedConjunction
-                }.Call(), 2);
-                summary.T4 = Math.Round(new DegreeOfAppropriateness()
-                {
-                    T3 = summary.T3,
-                    Summarizer1 = SelectedSummarizer,
-                    Summarizer2 = SelectedSecondSummarizer,
-                    ValuesForSummarizer1 = ValuesForSummarizer1,
-                    ValuesForSummarizer2 = ValuesForSummarizer2
-                }.Call(), 2);
-                summary.CalculateT();
+                    Description = sum,
+                    T1 = Math.Round(t1.Call(), 2),
+                    T2 = Math.Round(t2.Call(), 2),
+                    T3 = Math.Round(t3.Call(), 2),
+                    T4 = Math.Round(t4.Call(), 2),
 
+                    T5 = Math.Round(t5.Call(), 2),
+                    T6 = Math.Round(t6.Call(), 2),
+                    T7 = Math.Round(t7.Call(), 2),
+                    T8 = Math.Round(t8.Call(), 2),
+                    T9 = Math.Round(t9.Call(), 2),
+                    T10 = Math.Round(t10.Call(), 2),
+                    T11 = Math.Round(t11.Call(), 2),
+                };
+                summary.CalculateT();
                 Summaries.Add(summary);
             }
+
         }
+
+        private string GetSummary(Quantifier quantifier, bool hasQualifier, bool hasTwoSummarizers, CovertypeConverter convert)
+        {
+            var ret = $"{quantifier} {convert.Convert(SelectedSubject, null, null, null)}";
+
+            if (hasQualifier)
+            {
+                ret += $" BEING/HAVING {SelectedQualifier}";
+            }
+
+            ret += $" ARE / HAVE {SelectedSummarizer}";
+
+            if (hasTwoSummarizers)
+            {
+                ret += $" {SelectedConjunction} {SelectedSecondSummarizer}";
+            }
+            ret = ret.Replace(" Trapezoidal", String.Empty);
+            ret = ret.Replace(" Triangular", String.Empty);
+
+            return ret;
+        }
+        
     }
 }
